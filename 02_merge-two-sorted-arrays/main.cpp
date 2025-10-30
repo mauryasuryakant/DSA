04f440527ebc3705136d8b36671deeff06e858c1
Maurya Suryakant <mauryasuryakant99@gmail.com>
2025-10-30 20:22:01 +0530

    Completed 2nd

---
+++ 02_merge-two-sorted-arrays/main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+
+int main () {
+
+    std::vector<int> array_1 = {1, 2, 3, 4, 5};
+    std::vector<int> array_2 = {1, 2, 3, 4, 5};
+    std::vector<int> sorted;
+
+    int n1 = array_1.size();
+    int n2 = array_2.size();
+
+    for(int i = 0; i < n1; i++){
+        sorted.push_back(array_1[i]);
+    }
+
+    for(int i = 0; i < n2; i++){
+        sorted.push_back(array_2[i]);
+    }
+
+    for(int val : sorted){
+        std::cout << val << " ";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+// #include <vector>
+
+// int *MergeArray (static int array_1[],static int array_2[], int n) {
+
+//     std::vector<int> sorted;
+
+//     for(int i = 0; i < n; i++){
+//         sorted.push_back(array_1[i]);
+//     }
+
+//     for(int i = 0; i < n; i++){
+//         sorted.push_back(array_1[i]);
+//     }
+
+//     return sorted;
+
+// }
+
+
+
